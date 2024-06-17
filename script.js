91d0106b5617614225a9916bf2527acfeef938e6
Ashmita-1 <146411027+Ashmita-1@users.noreply.github.com>
2024-06-17 17:55:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
 import * as dat from 'dat.gui';
+import Nebula from '../Nebula_Image.jpeg';
+import Star from '../Stars_Image.jpg';
 
 const renderer = new THREE.WebGLRenderer();
+
+renderer.shadowMap.enabled = true;
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 
@@ -26,38 +30,88 @@ camera.position.set(-10, 30, 30);
 orbit.update();
 
 const boxGeometry = new THREE.BoxGeometry();
-const boxMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00});
+const boxMaterial = new THREE.MeshBasicMaterial({color: 0x00FF00});
 const box = new THREE.Mesh(boxGeometry, boxMaterial);
 scene.add(box);
 
 const planeGeometry = new THREE.PlaneGeometry(30, 30);
-const planeMaterial = new THREE.MeshBasicMaterial({
+const planeMaterial = new THREE.MeshStandardMaterial({
     color: 0xffffff,
     side: THREE.DoubleSide
 });
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 scene.add(plane);
 plane.rotation.x = -0.5 * Math.PI;
+plane.receiveShadow = true;
 
 const gridHelper = new THREE.GridHelper(30);
 scene.add(gridHelper);
 
 const sphereGeometry = new THREE.SphereGeometry(4, 50, 50);
-const sphereMaterial = new THREE.MeshBasicMaterial({
-    color: 0x0000ff,
+const sphereMaterial = new THREE.MeshStandardMaterial({
+    color: 0x0000FF,
     wireframe: false
 });
 const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 scene.add(sphere);
+sphere.position.set(-10, 10, 0);
+sphere.castShadow = true;
 
-sphere.position.set(-10, 10, 0)
+const ambientLight = new THREE.AmbientLight(0x333333);
+scene.add(ambientLight);
+
+// const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+// scene.add(directionalLight);
+// directionalLight.position.set(-30, 50, 0);
+// directionalLight.castShadow = true;
+// directionalLight.shadow.camera.bottom = -12;
+
+// const dLightHelper = new THREE.DirectionalLightHelper(directionalLight, 5);
+// scene.add(dLightHelper);
+
+// const dLightShadowHelper = new THREE.CameraHelper(directionalLight.shadow.camera);
+// scene.add(dLightShadowHelper);
+
+const spotLight = new THREE.SpotLight(0xFFFFFF, 5000);
+scene.add(spotLight);
+spotLight.position.set(-100, 100, 0);
+spotLight.castShadow = true;
+spotLight.angle = 0.2;
+
+const sLightHelper = new THREE.SpotLightHelper(spotLight);
+scene.add(sLightHelper);
+
+// scene.fog = new THREE.Fog(0xffffff, 0, 200);
+scene.fog = new THREE.FogExp2(0xffffff, 0.01);
+
+// renderer.setClearColor(0xffea00);
+const textureLoader = new THREE.TextureLoader();
+scene.background = textureLoader.load(Star);
+// const cubeTextureLoader = new THREE.cubeTextureLoader();
+// scene.background = cubeTextureLoader.load([
+//     Nebula,
+//     Nebula,
+//     Star,
+//     Star,
+//     Star,
+//     Star
+// ]);
+
+const box2Geometry = new THREE.BoxGeometry(4, 4, 4);
+const box2Material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+const box2 = new THREE.Mesh(box2Geometry, box2Material);
+scene.add(box2);
+box2.position.set(0, 15, 10);
 
 const gui = new dat.GUI();
 
 const options = {
-    sphereColor: '#ffea00',
+    sphereColor: '#FFEA00',
     wireframe: false,
-    speed: 0.01 
+    speed: 0.01  ,
+    angle: 0.2,
+    penumbra: 0,
+    intensity: 1
 };
 
 gui.addColor(options, 'sphereColor').onChange(function(e){
@@ -68,14 +122,24 @@ gui.add(options, 'wireframe').onChange(function(e){
     sphere.material.wireframe = e;
 });
 
+gui.add(options, 'speed', 0, 0.1);
+gui.add(options, 'angle', 0, 1);
+gui.add(options, 'penumbra', 0, 1);
+gui.add(options, 'intensity', 0, 1);
+
 let step = 0;
 
 function animate(time) {
     box.rotation.x = time / 1000;
     box.rotation.y = time / 1000;
    
-    step += speed;
+    step += options.speed;
     sphere.position.y = 10 * Math.abs(Math.sin(step));
+
+    spotLight.angle = options.angle;
+    spotLight.penumbra = options.penumbra;
+    spotLight.intensity = options.intensity;
+    sLightHelper.update();
 
     renderer.render(scene, camera);
 }
